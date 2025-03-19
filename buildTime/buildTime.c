7b3607e1bfeb705efd8114d8c6ce8b36f43ce217
l <lgch@lyjpbc.com>
2025-03-19 16:05:41 +0800

    update buildTime module

--- buildTime/buildTime.c
+++ buildTime/buildTime.c
@@ -2,102 +2,29 @@
 #include <sys/time.h>
 #include <string.h>
 #include <time.h>
+#include "build_defs.h"
 
-const char Software_Ver[]   = "Software: 1.0.0";
+const char Software_Ver[]   = "Software: 1.1.0";
 const char Compiler_Date[]  = "Date: "__DATE__;
 const char Compiler_Time[]  = "Time: "__TIME__;
-
-#define YEAR_I ((((__DATE__ [7] - '0') * 10 + (__DATE__ [8] - '0')) * 10 \
-    + (__DATE__ [9] - '0')) * 10 + (__DATE__ [10] - '0'))
-
-#define MONTH_I (__DATE__ [2] == 'n' ? (__DATE__ [1] == 'a' ? 1 : 6) \
-    : __DATE__ [2] == 'b' ? 2 \
-    : __DATE__ [2] == 'r' ? (__DATE__ [0] == 'M' ? 3 : 4) \
-    : __DATE__ [2] == 'y' ? 5 \
-    : __DATE__ [2] == 'l' ? 7 \
-    : __DATE__ [2] == 'g' ? 8 \
-    : __DATE__ [2] == 'p' ? 9 \
-    : __DATE__ [2] == 't' ? 10 \
-    : __DATE__ [2] == 'v' ? 11 : 12)
-
-#define DAY_I ((__DATE__ [4] == ' ' ? 0 : __DATE__ [4] - '0') * 10 \
-    + (__DATE__ [5] - '0'))
-
-#define DIGIT_TO_STR(n) \
-    ((n) == 0 ? "0" : \
-    (n) == 1 ? "1" : \
-    (n) == 2 ? "2" : \
-    (n) == 3 ? "3" : \
-    (n) == 4 ? "4" : \
-    (n) == 5 ? "5" : \
-    (n) == 6 ? "6" : \
-    (n) == 7 ? "7" : \
-    (n) == 8 ? "8" : \
-    (n) == 9 ? "9" : "Invalid Digit")
-
-#define YEAR_S (__DATE__+7)
-
-#define MONTH_S (__DATE__ [2] == 'n' ? (__DATE__ [1] == 'a' ? "01" : "06") \
-    : __DATE__ [2] == 'b' ? "02" \
-    : __DATE__ [2] == 'r' ? (__DATE__ [0] == 'M' ? "03" : "04") \
-    : __DATE__ [2] == 'y' ? "05" \
-    : __DATE__ [2] == 'l' ? "07" \
-    : __DATE__ [2] == 'g' ? "08" \
-    : __DATE__ [2] == 'p' ? "09" \
-    : __DATE__ [2] == 't' ? "10" \
-    : __DATE__ [2] == 'v' ? "11" : "12")
-
-//#define DAY_S (__DATE__[4] == ' ' ? \
-                    (__DATE__[5] == '1' ? "01" : \
-                    (__DATE__[5] == '2' ? "02" : \
-                    (__DATE__[5] == '3' ? "03" : \
-                    (__DATE__[5] == '4' ? "04" : \
-                    (__DATE__[5] == '5' ? "05" : \
-                    (__DATE__[5] == '6' ? "06" : \
-                    (__DATE__[5] == '7' ? "07" : \
-                    (__DATE__[5] == '8' ? "08" : \
-                    (__DATE__[5] == '9' ? "09"))))))))) \
-            : (__DATE__[4] == '1' ? \
-                    (__DATE__[5] == '0' ? "10" : \
-                    (__DATE__[5] == '1' ? "11" : \
-                    (__DATE__[5] == '2' ? "12" : \
-                    (__DATE__[5] == '3' ? "13" : \
-                    (__DATE__[5] == '4' ? "14" : \
-                    (__DATE__[5] == '5' ? "15" : \
-                    (__DATE__[5] == '6' ? "16" : \
-                    (__DATE__[5] == '7' ? "17" : \
-                    (__DATE__[5] == '8' ? "18" : \
-                    (__DATE__[5] == '9' ? "19"))))))))) \
-            : (__DATE__[4] == '2' ? \
-                    (__DATE__[5] == '0' ? "20" : \
-                    (__DATE__[5] == '1' ? "21" : \
-                    (__DATE__[5] == '2' ? "22" : \
-                    (__DATE__[5] == '3' ? "23" : \
-                    (__DATE__[5] == '4' ? "24" : \
-                    (__DATE__[5] == '5' ? "25" : \
-                    (__DATE__[5] == '6' ? "26" : \
-                    (__DATE__[5] == '7' ? "27" : \
-                    (__DATE__[5] == '8' ? "28" : \
-                    (__DATE__[5] == '9' ? "29"))))))))) \
-            : (__DATE__[4] == '3' ? \
-                    (__DATE__[5] == '0' ? "30" : \
-                    (__DATE__[5] == '1' ? "31"))))))))
-#define DAY_S ""
-
-#define DAY_S_S DIGIT_TO_STR(__DATE__ [4] == ' ' ? 0 : __DATE__ [4] - '0')
-#define DAY_G_S DIGIT_TO_STR(__DATE__ [5] - '0')
-
 
 int main()
 {
     printf("Software_Ver:[%s]\n", Software_Ver);
     printf("Compiler_Date:[%s]\n", Compiler_Date);
     printf("Compiler_Time:[%s]\n", Compiler_Time);
-    printf("YEAR_I:[%d], MONTH_I:[%d], DAY_I:[%d]\n", YEAR_I, MONTH_I, DAY_I);
-    printf("YEAR_S:[%s], MONTH_S:[%s], DAY_S:[%s], DAY_S_S:[%s], DAY_G_S:[%s]\n", YEAR_S, MONTH_S, DAY_S, DAY_S_S, DAY_G_S);
 
-    unsigned int wCompileData = (YEAR_I%100)*10000 + (MONTH_I%100)*100 + DAY_I;
-    printf("wCompileData:[%u]\n", wCompileData);
+    printf("BUILD_YEAR_I:[%d], BUILD_MONTH_I:[%d], BUILD_DAY_I:[%d]\n", BUILD_YEAR_I, BUILD_MONTH_I, BUILD_DAY_I);
+    printf("BUILD_HOUR_I:[%d], BUILD_MIN_I:[%d], BUILD_SEC_I:[%d]\n", BUILD_HOUR_I, BUILD_MIN_I, BUILD_SEC_I);
+    unsigned int iBuildData = (BUILD_YEAR_I%100)*10000 + (BUILD_MONTH_I%100)*100 + BUILD_DAY_I;
+    unsigned int iBuildTime = (BUILD_HOUR_I%100)*10000 + (BUILD_MIN_I%100)*100 + BUILD_SEC_I;
+    printf("iBuildData:[%u], iBuildTime:[%d]\n", iBuildData, iBuildTime);
 
+    printf("BUILD_YEAR_S:[%s], BUILD_MONTH_S:[%s], BUILD_DAY_S:[%s]\n", BUILD_YEAR_S, BUILD_MONTH_S, BUILD_DAY_S);
+    printf("BUILD_HOUR_S:[%s], BUILD_MIN_S:[%s], BUILD_SEC_S:[%s]\n", BUILD_HOUR_S, BUILD_MIN_S, BUILD_SEC_S);
+    char sBuildData[9] = {0}, sBuildTime[7] = {0};
+    snprintf(sBuildData, sizeof(sBuildData), "%s%s%s", BUILD_YEAR_S, BUILD_MONTH_S, BUILD_DAY_S);
+    snprintf(sBuildTime, sizeof(sBuildTime), "%s%s%s", BUILD_HOUR_S, BUILD_MIN_S, BUILD_SEC_S);
+    printf("sBuildData:[%s], sBuildTime:[%s]\n", sBuildData, sBuildTime);
     return 0;
 }
--- buildTime/build_defs.h
+++ buildTime/build_defs.h
@@ -1,35 +1,47 @@
 /**
  * @file build_defs.h (only one file).
- * @brief 转换编译时间为数字格式
- * @date 2022.4.15
- * @author William
+ * @brief 转换编译时间为数字格式以及独立字符形式。
+ * @brief 由于部分宏使用复合字面量的解决方案，且因为__DATE__[*]和__TIME__[*]是运行时字符值。（区别于__DATE__和__TIME__）
+ * @brief 因此该文件大部分宏不可在函数外全局使用​（例如全局变量初始化），但函数内安全。
+ * @date 2025.3.19
+ * @author Xiaoman
+ * @version 1.0
+*/
+
+// Example of __DATE__ string: "Mar  2 2025"
+// Example of __DATE__ string: "Mar 19 2025"
+// Example of __TIME__ string: "14:36:54"
+// Example of __TIME__ string: "06:08:04"
+/*
+    unsigned int iBuildData = (BUILD_YEAR_I%100)*10000 + (BUILD_MONTH_I%100)*100 + BUILD_DAY_I;
+    unsigned int iBuildTime = (BUILD_HOUR_I%100)*10000 + (BUILD_MIN_I%100)*100 + BUILD_SEC_I;
+    char sBuildData[9] = {0}, sBuildTime[7] = {0};
+    snprintf(sBuildData, sizeof(sBuildData), "%s%s%s", BUILD_YEAR_S, BUILD_MONTH_S, BUILD_DAY_S);
+    snprintf(sBuildTime, sizeof(sBuildTime), "%s%s%s", BUILD_HOUR_S, BUILD_MIN_S, BUILD_SEC_S);
 */
 
 #ifndef BUILD_DEFS_H
-
 #define BUILD_DEFS_H
 
-// Example of __DATE__ string: "Jul 27 2012"
-// Example of __TIME__ string: "21:06:19"
+#if 1
+// 月份判断宏
+#define MONTH_CMP(a,b,c) (__DATE__[0]==a && __DATE__[1]==b && __DATE__[2]==c)
 
-// Use it: printf("%04d-%02d-%02dT%02d:%02d:%02d\n", BUILD_YEAR, BUILD_MONTH, BUILD_DAY, BUILD_HOUR, BUILD_MIN, BUILD_SEC);
-
-#define COMPUTE_BUILD_YEAR \
-    ( \
-        (__DATE__[ 7] - '0') * 1000 + \
-        (__DATE__[ 8] - '0') *  100 + \
-        (__DATE__[ 9] - '0') *   10 + \
-        (__DATE__[10] - '0') \
-    )
-
-
-#define COMPUTE_BUILD_DAY \
-    ( \
-        ((__DATE__[4] >= '0') ? (__DATE__[4] - '0') * 10 : 0) + \
-        (__DATE__[5] - '0') \
-    )
-
-
+//十二个月的字符判断宏定义
+#define BUILD_MONTH_IS_JAN MONTH_CMP('J','a','n')
+#define BUILD_MONTH_IS_FEB MONTH_CMP('F','e','b')
+#define BUILD_MONTH_IS_MAR MONTH_CMP('M','a','r')
+#define BUILD_MONTH_IS_APR MONTH_CMP('A','p','r')
+#define BUILD_MONTH_IS_MAY MONTH_CMP('M','a','y')
+#define BUILD_MONTH_IS_JUN MONTH_CMP('J','u','n')
+#define BUILD_MONTH_IS_JUL MONTH_CMP('J','u','l')
+#define BUILD_MONTH_IS_AUG MONTH_CMP('A','u','g')
+#define BUILD_MONTH_IS_SEP MONTH_CMP('S','e','p')
+#define BUILD_MONTH_IS_OCT MONTH_CMP('O','c','t')
+#define BUILD_MONTH_IS_NOV MONTH_CMP('N','o','v')
+#define BUILD_MONTH_IS_DEC MONTH_CMP('D','e','c')
+#else//更巧妙，但不易读的写法
+//十二个月的字符判断宏定义
 #define BUILD_MONTH_IS_JAN (__DATE__[0] == 'J' && __DATE__[1] == 'a' && __DATE__[2] == 'n')
 #define BUILD_MONTH_IS_FEB (__DATE__[0] == 'F')
 #define BUILD_MONTH_IS_MAR (__DATE__[0] == 'M' && __DATE__[1] == 'a' && __DATE__[2] == 'r')
@@ -42,9 +54,23 @@
 #define BUILD_MONTH_IS_OCT (__DATE__[0] == 'O')
 #define BUILD_MONTH_IS_NOV (__DATE__[0] == 'N')
 #define BUILD_MONTH_IS_DEC (__DATE__[0] == 'D')
+#endif
 
+#define BUILD_DATE_IS_BAD (__DATE__[0] == '?')
+#define BUILD_TIME_IS_BAD (__TIME__[0] == '?')
+/*===============================================================================================*/
+/*====================================数字形式宏定义(Start)=======================================*/
+//年月日的数字形式宏定义
+#define COMPUTE_BUILD_YEAR_I \
+    ( \
+        (__DATE__[ 7] - '0') * 1000 + \
+        (__DATE__[ 8] - '0') *  100 + \
+        (__DATE__[ 9] - '0') *   10 + \
+        (__DATE__[10] - '0') \
+    )
 
-#define COMPUTE_BUILD_MONTH \
+#if 1
+#define COMPUTE_BUILD_MONTH_I \
     ( \
         (BUILD_MONTH_IS_JAN) ?  1 : \
         (BUILD_MONTH_IS_FEB) ?  2 : \
@@ -58,26 +84,127 @@
         (BUILD_MONTH_IS_OCT) ? 10 : \
         (BUILD_MONTH_IS_NOV) ? 11 : \
         (BUILD_MONTH_IS_DEC) ? 12 : \
-        /* error default */  99 \
+        /* error default */  0 \
+    )
+#else//更巧妙，但不易读的写法
+#define COMPUTE_BUILD_MONTH_I (__DATE__[2] == 'n' ? (__DATE__[1] == 'a' ? 1 : 6) \
+    : __DATE__[2] == 'b' ? 2 \
+    : __DATE__[2] == 'r' ? (__DATE__[0] == 'M' ? 3 : 4) \
+    : __DATE__[2] == 'y' ? 5 \
+    : __DATE__[2] == 'l' ? 7 \
+    : __DATE__[2] == 'g' ? 8 \
+    : __DATE__[2] == 'p' ? 9 \
+    : __DATE__[2] == 't' ? 10 \
+    : __DATE__[2] == 'v' ? 11 : 12)
+#endif
+
+#define COMPUTE_BUILD_DAY_I \
+    ( \
+        ((__DATE__[4] >= '0') ? (__DATE__[4] - '0') * 10 : 0) + \
+        (__DATE__[5] - '0') \
     )
 
-#define COMPUTE_BUILD_HOUR ((__TIME__[0] - '0') * 10 + __TIME__[1] - '0')
-#define COMPUTE_BUILD_MIN  ((__TIME__[3] - '0') * 10 + __TIME__[4] - '0')
-#define COMPUTE_BUILD_SEC  ((__TIME__[6] - '0') * 10 + __TIME__[7] - '0')
+#define BUILD_YEAR_I  ((BUILD_DATE_IS_BAD) ? 0 : COMPUTE_BUILD_YEAR_I)
+#define BUILD_MONTH_I ((BUILD_DATE_IS_BAD) ? 0 : COMPUTE_BUILD_MONTH_I)
+#define BUILD_DAY_I   ((BUILD_DATE_IS_BAD) ? 0 : COMPUTE_BUILD_DAY_I)
+/*===============================================================================================*/
+#define COMPUTE_BUILD_HOUR_I ((__TIME__[0] - '0') * 10 + __TIME__[1] - '0')
+#define COMPUTE_BUILD_MIN_I  ((__TIME__[3] - '0') * 10 + __TIME__[4] - '0')
+#define COMPUTE_BUILD_SEC_I  ((__TIME__[6] - '0') * 10 + __TIME__[7] - '0')
 
+#define BUILD_HOUR_I  ((BUILD_TIME_IS_BAD) ? 0 :  COMPUTE_BUILD_HOUR_I)
+#define BUILD_MIN_I   ((BUILD_TIME_IS_BAD) ? 0 :  COMPUTE_BUILD_MIN_I)
+#define BUILD_SEC_I   ((BUILD_TIME_IS_BAD) ? 0 :  COMPUTE_BUILD_SEC_I)
+/*======================================数字形式宏定义(End)=======================================*/
+/*===============================================================================================*/
 
-#define BUILD_DATE_IS_BAD (__DATE__[0] == '?')
+/*===============================================================================================*/
+/*====================================字符形式宏定义(Start)=======================================*/
+//年月日的字符形式宏定义
+#define COMPUTE_BUILD_YEAR_S (&(__DATE__[7]))
 
-#define BUILD_YEAR  ((BUILD_DATE_IS_BAD) ? 99 : COMPUTE_BUILD_YEAR)
-#define BUILD_MONTH ((BUILD_DATE_IS_BAD) ? 99 : COMPUTE_BUILD_MONTH)
-#define BUILD_DAY   ((BUILD_DATE_IS_BAD) ? 99 : COMPUTE_BUILD_DAY)
+#if 1
+#define COMPUTE_BUILD_MONTH_S \
+    ( \
+        (BUILD_MONTH_IS_JAN) ? "01" : \
+        (BUILD_MONTH_IS_FEB) ? "02" : \
+        (BUILD_MONTH_IS_MAR) ? "03" : \
+        (BUILD_MONTH_IS_APR) ? "04" : \
+        (BUILD_MONTH_IS_MAY) ? "05" : \
+        (BUILD_MONTH_IS_JUN) ? "06" : \
+        (BUILD_MONTH_IS_JUL) ? "07" : \
+        (BUILD_MONTH_IS_AUG) ? "08" : \
+        (BUILD_MONTH_IS_SEP) ? "09" : \
+        (BUILD_MONTH_IS_OCT) ? "10" : \
+        (BUILD_MONTH_IS_NOV) ? "11" : \
+        (BUILD_MONTH_IS_DEC) ? "12" : \
+        /* error default */  "00" \
+    )
+#else//更巧妙，但不易读的写法
+#define COMPUTE_BUILD_MONTH_S (__DATE__[2] == 'n' ? (__DATE__[1] == 'a' ? "01" : "06") \
+    : __DATE__[2] == 'b' ? "02" \
+    : __DATE__[2] == 'r' ? (__DATE__[0] == 'M' ? "03" : "04") \
+    : __DATE__[2] == 'y' ? "05" \
+    : __DATE__[2] == 'l' ? "07" \
+    : __DATE__[2] == 'g' ? "08" \
+    : __DATE__[2] == 'p' ? "09" \
+    : __DATE__[2] == 't' ? "10" \
+    : __DATE__[2] == 'v' ? "11" : "12")
+#endif
 
-#define BUILD_TIME_IS_BAD (__TIME__[0] == '?')
+#if 1
+#define COMPUTE_BUILD_DAY_S ((char[]){ (__DATE__[4] == ' ' ? '0' : __DATE__[4]), __DATE__[5], '\0' })
+#else// 更简洁易读的写法
+// 天数判断宏
+#define DAY_CMP(a,b) (__DATE__[4]==a && __DATE__[5]==b)
 
-#define BUILD_HOUR  ((BUILD_TIME_IS_BAD) ? 99 :  COMPUTE_BUILD_HOUR)
-#define BUILD_MIN   ((BUILD_TIME_IS_BAD) ? 99 :  COMPUTE_BUILD_MIN)
-#define BUILD_SEC   ((BUILD_TIME_IS_BAD) ? 99 :  COMPUTE_BUILD_SEC)
+// 天数字符转换
+#define COMPUTE_BUILD_DAY_S \
+(DAY_CMP(' ','1') ? "01" :  \
+ DAY_CMP(' ','2') ? "02" :  \
+ DAY_CMP(' ','3') ? "03" :  \
+ DAY_CMP(' ','4') ? "04" :  \
+ DAY_CMP(' ','5') ? "05" :  \
+ DAY_CMP(' ','6') ? "06" :  \
+ DAY_CMP(' ','7') ? "07" :  \
+ DAY_CMP(' ','8') ? "08" :  \
+ DAY_CMP(' ','9') ? "09" :  \
+ DAY_CMP('1','0') ? "10" :  \
+ DAY_CMP('1','1') ? "11" :  \
+ DAY_CMP('1','2') ? "12" :  \
+ DAY_CMP('1','3') ? "13" :  \
+ DAY_CMP('1','4') ? "14" :  \
+ DAY_CMP('1','5') ? "15" :  \
+ DAY_CMP('1','6') ? "16" :  \
+ DAY_CMP('1','7') ? "17" :  \
+ DAY_CMP('1','8') ? "18" :  \
+ DAY_CMP('1','9') ? "19" :  \
+ DAY_CMP('2','0') ? "20" :  \
+ DAY_CMP('2','1') ? "21" :  \
+ DAY_CMP('2','2') ? "22" :  \
+ DAY_CMP('2','3') ? "23" :  \
+ DAY_CMP('2','4') ? "24" :  \
+ DAY_CMP('2','5') ? "25" :  \
+ DAY_CMP('2','6') ? "26" :  \
+ DAY_CMP('2','7') ? "27" :  \
+ DAY_CMP('2','8') ? "28" :  \
+ DAY_CMP('2','9') ? "29" :  \
+ DAY_CMP('3','0') ? "30" :  \
+ DAY_CMP('3','1') ? "31" : "00")
+#endif
 
+#define BUILD_YEAR_S  ((BUILD_DATE_IS_BAD) ? "00" : COMPUTE_BUILD_YEAR_S)
+#define BUILD_MONTH_S ((BUILD_DATE_IS_BAD) ? "00" : COMPUTE_BUILD_MONTH_S)
+#define BUILD_DAY_S   ((BUILD_DATE_IS_BAD) ? "00" : COMPUTE_BUILD_DAY_S)
+/*===============================================================================================*/
+#define COMPUTE_BUILD_HOUR_S ((char[]){ __TIME__[0], __TIME__[1], '\0' })
+#define COMPUTE_BUILD_MIN_S  ((char[]){ __TIME__[3], __TIME__[4], '\0' })
+#define COMPUTE_BUILD_SEC_S  ((char[]){ __TIME__[6], __TIME__[7], '\0' })
 
+#define BUILD_HOUR_S  ((BUILD_TIME_IS_BAD) ? "00" :  COMPUTE_BUILD_HOUR_S)
+#define BUILD_MIN_S   ((BUILD_TIME_IS_BAD) ? "00" :  COMPUTE_BUILD_MIN_S)
+#define BUILD_SEC_S   ((BUILD_TIME_IS_BAD) ? "00" :  COMPUTE_BUILD_SEC_S)
+/*======================================字符形式宏定义(End)=======================================*/
+/*===============================================================================================*/
 #endif // BUILD_DEFS_H
 
